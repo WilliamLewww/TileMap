f4db90ca5ff6eddb10e5b18aaeb0ba82f7455a03
WilliamLewww <WilliamLew32@gmail.com>
2016-06-11 20:18:25 -0700

    intial

---
+++ Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TileMap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CreateGraphics().Clear(Color.White);
+            CreateGraphics().DrawLine(new Pen(Color.Black), 10, 50, (float)numericUpDown1.Value + 10, 50);
+            CreateGraphics().DrawLine(new Pen(Color.Black), 10, 50, 10, (float)numericUpDown2.Value + 50);
+            CreateGraphics().DrawLine(new Pen(Color.Black), (float)numericUpDown1.Value + 10, 50, (float)numericUpDown1.Value + 10, (float)numericUpDown2.Value + 50);
+            CreateGraphics().DrawLine(new Pen(Color.Black), 10, (float)numericUpDown2.Value + 50, (float)numericUpDown1.Value + 10, (float)numericUpDown2.Value + 50);
+        }
+    }
+}
