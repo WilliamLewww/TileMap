baccfe909831195fbc8988e820f3bd26084977e7
WilliamLewww <WilliamLew32@gmail.com>
2016-06-13 19:14:29 -0700

    finished saving, loading, and displaying tilemap

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,13 +31,23 @@
             this.button1 = new System.Windows.Forms.Button();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -47,7 +57,12 @@
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(95, 13);
+            this.numericUpDown1.Location = new System.Drawing.Point(95, 12);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            5000,
+            0,
+            0,
+            0});
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(55, 20);
             this.numericUpDown1.TabIndex = 1;
@@ -55,16 +70,93 @@
             // numericUpDown2
             // 
             this.numericUpDown2.Location = new System.Drawing.Point(156, 12);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            5000,
+            0,
+            0,
+            0});
             this.numericUpDown2.Name = "numericUpDown2";
             this.numericUpDown2.Size = new System.Drawing.Size(55, 20);
             this.numericUpDown2.TabIndex = 2;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(217, 12);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            5000,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(37, 20);
+            this.numericUpDown3.TabIndex = 3;
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(260, 12);
+            this.numericUpDown4.Maximum = new decimal(new int[] {
+            5000,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(37, 20);
+            this.numericUpDown4.TabIndex = 4;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1001, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Load";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1001, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Save As";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1001, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Save";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(858, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(137, 20);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.Text = "File Name";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.ClientSize = new System.Drawing.Size(720, 261);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1088, 837);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.numericUpDown3);
             this.Controls.Add(this.numericUpDown2);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.button1);
@@ -72,7 +164,10 @@
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -81,6 +176,14 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace TileMap
 {
+    enum Tile : int
+    {
+        RegularTile = 1,
+        PushTile = 2,
+        EndPushTile = 3,
+        SpawnTile = 4,
+        HarmfulTile = 5,
+        TrampolineTile = 6,
+        SetTrampolineTile = 7,
+        ReverseSetTrampolineTile = 8,
+        MovingTile = 9,
+        HalfMovingTile = 10,
+        ReverseHalfMovingTile = 11
+    };
+
     public partial class Form1 : Form
     {
         public Form1()
@@ -19,11 +30,163 @@ namespace TileMap
 
         private void button1_Click(object sender, EventArgs e)
         {
-            CreateGraphics().Clear(Color.White);
-            CreateGraphics().DrawLine(new Pen(Color.Black), 10, 50, (float)numericUpDown1.Value + 10, 50);
-            CreateGraphics().DrawLine(new Pen(Color.Black), 10, 50, 10, (float)numericUpDown2.Value + 50);
-            CreateGraphics().DrawLine(new Pen(Color.Black), (float)numericUpDown1.Value + 10, 50, (float)numericUpDown1.Value + 10, (float)numericUpDown2.Value + 50);
-            CreateGraphics().DrawLine(new Pen(Color.Black), 10, (float)numericUpDown2.Value + 50, (float)numericUpDown1.Value + 10, (float)numericUpDown2.Value + 50);
+            GenerateGrid((int)numericUpDown1.Value, (int)numericUpDown2.Value, (int)numericUpDown3.Value, (int)numericUpDown4.Value);
+        }
+
+        void GenerateGrid(int width, int height, int divisionsX, int divisionsY)
+        {
+            tileMap = new int[divisionsX, divisionsY];
+
+            CreateGraphics().Clear(Color.Gray);
+            CreateGraphics().DrawLine(new Pen(Color.Black), 10, 50, width + 10, 50);
+            CreateGraphics().DrawLine(new Pen(Color.Black), 10, 50, 10, height + 50);
+            CreateGraphics().DrawLine(new Pen(Color.Black), width + 10, 50, width + 10, height + 50);
+            CreateGraphics().DrawLine(new Pen(Color.Black), 10, height + 50, width + 10, height + 50);
+
+            for (int x = 0; x < divisionsX - 1; x++)
+            {
+                CreateGraphics().DrawLine(new Pen(Color.Black), 10 + ((width / divisionsX) * (1 + x)), 50, 10 + ((width / divisionsX) * (1 + x)), height + 50);
+            }
+
+            for (int y = 0; y < divisionsY - 1; y++)
+            {
+                CreateGraphics().DrawLine(new Pen(Color.Black), 10, 50 + ((height / divisionsY) * (1 + y)), width + 10, 50 + ((height / divisionsY) * (1 + y)));
+            }
+        }
+
+        void GenerateTile(int x, int y, int width, int height, Color color)
+        {
+            CreateGraphics().FillRectangle(new SolidBrush(color), x + 11, y + 51, width - 1, height - 1);
+        }
+
+        static int[,] tileMap;
+        List<string> stringList = new List<string>();
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+
+            if (openFileDialog1.FileName.Substring(openFileDialog1.FileName.LastIndexOf('.')).Equals(".txt"))
+            {
+                savePath = openFileDialog1.FileName;
+                textBox1.Text = openFileDialog1.FileName.Substring(openFileDialog1.FileName.LastIndexOf('\\') + 1).TrimEnd(".txt".ToCharArray());
+
+                using (StreamReader sr = File.OpenText(openFileDialog1.FileName))
+                {
+                    stringList.Clear();
+                    string s = "";
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        stringList.Add(s);
+                    }
+                }
+            }
+
+            int increment = 0;
+            for (int x = 0; x < stringList[0].Length; x++)
+            {
+                if (stringList[0][x] == ',') increment += 1;
+            }
+
+            GenerateGrid(800, 600, increment, stringList.Count);
+            increment = 0;
+
+            string intString = "";
+
+            for (int y = 0; y < tileMap.GetLength(1); y++)
+            {
+                increment = 0;
+                for (int x = 0; x < stringList[y].Length; x++)
+                {
+                    if (stringList[y][x] == ',')
+                    {
+                        tileMap[increment, y] = int.Parse(intString);
+                        intString = "";
+                        increment += 1;
+                    }
+                    else
+                    {
+                        if (stringList[y][x] != '{' && stringList[y][x] != '}')
+                        {
+                            intString += stringList[y][x];
+                        }
+                    }
+                }
+            }
+
+            for (int y = 0; y < tileMap.GetLength(1); y++)
+            {
+                for (int x = 0; x < tileMap.GetLength(0); x++)
+                {
+                    if (tileMap[x, y] == 1) GenerateTile(x * 10, y * 10, 10, 10, Color.Blue);
+                    if (tileMap[x, y] == 2) GenerateTile(x * 10, y * 10, 10, 10, Color.Pink);
+                    if (tileMap[x, y] == 3) GenerateTile(x * 10, y * 10, 10, 10, Color.Pink);
+                    if (tileMap[x, y] == 4) GenerateTile(x * 10, y * 10, 10, 10, Color.Black);
+                    if (tileMap[x, y] == 5) GenerateTile(x * 10, y * 10, 10, 10, Color.Red);
+                    if (tileMap[x, y] == 6) GenerateTile(x * 10, (y * 10) + 5, 10, 5, Color.HotPink);
+                    if (tileMap[x, y] == 7) GenerateTile(x * 10, (y * 10) + 5, 10, 5, Color.HotPink);
+                    if (tileMap[x, y] == 8) GenerateTile(x * 10, y * 10, 10, 5, Color.HotPink);
+                    if (tileMap[x, y] == 9) GenerateTile(x * 10, y * 10, 10, 10, Color.Yellow);
+                    if (tileMap[x, y] == 10) GenerateTile(x * 10, (y * 10) + 5, 10, 5, Color.Yellow);
+                    if (tileMap[x, y] == 11) GenerateTile(x * 10, y * 10, 10, 5, Color.Yellow);
+                }
+            }
+        }
+
+        string savePath = "";
+        private void button3_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            savePath = folderBrowserDialog1.SelectedPath;
+            savePath += "\\" + textBox1.Text + ".txt";
+
+            using (StreamWriter sw = File.CreateText(savePath))
+            {
+                for (int y = 0; y < tileMap.GetLength(1); y++)
+                {
+                    for (int x = 0; x < tileMap.GetLength(0); x++)
+                    {
+                        if (x == 0) sw.Write('{');
+                        sw.Write(tileMap[x, y]);
+                        if (x == tileMap.GetLength(0) - 1)
+                        {
+                            if (y == tileMap.GetLength(1) - 1) { sw.Write(" }"); }
+                            else { sw.Write(" },"); }
+                        }
+                        else { sw.Write(','); }
+                    }
+                    sw.WriteLine();
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (savePath.Equals(""))
+            {
+                folderBrowserDialog1.ShowDialog();
+                savePath = folderBrowserDialog1.SelectedPath;
+                savePath += "\\" + textBox1.Text + ".txt";
+            }
+
+            using (StreamWriter sw = File.CreateText(savePath))
+            {
+                for (int y = 0; y < tileMap.GetLength(1); y++)
+                {
+                    for (int x = 0; x < tileMap.GetLength(0); x++)
+                    {
+                        if (x == 0) sw.Write('{');
+                        sw.Write(tileMap[x, y]);
+                        if (x == tileMap.GetLength(0) - 1)
+                        {
+                            if (y == tileMap.GetLength(1) - 1) { sw.Write(" }"); }
+                            else { sw.Write(" },"); }
+                        }
+                        else { sw.Write(','); }
+                    }
+                    sw.WriteLine();
+                }
+            }
         }
     }
 }
